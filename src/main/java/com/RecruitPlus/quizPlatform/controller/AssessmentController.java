a178d39ca052be2d09aad14be212a41d60f2f1e9
radhikarudrawar <87385027+radhikarudrawar@users.noreply.github.com>
2022-08-03 22:01:09 +0530

    done

--- src/main/java/com/RecruitPlus/quizPlatform/controller/AssessmentController.java
+++ src/main/java/com/RecruitPlus/quizPlatform/controller/AssessmentController.java
@@ -1,14 +1,22 @@
 package com.RecruitPlus.quizPlatform.controller;
 
+import com.RecruitPlus.quizPlatform.model.Assessment;
 import com.RecruitPlus.quizPlatform.service.AssessmentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/assessment/v1")
+@RequestMapping("/assessments/v1")
 public class AssessmentController {
     @Autowired
     private AssessmentService assessmentService;
 
+    @PostMapping("/assessemet")
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Assessment saveAssessment(@RequestBody Assessment assessment)
+    {
+        Assessment saveAssessment = assessmentService.saveNewAssessment(assessment);
+        return saveAssessment;
+    }
 }
