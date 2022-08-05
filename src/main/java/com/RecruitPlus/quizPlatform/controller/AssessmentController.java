9a3ffb99ff657e030e874e2ed4d8855d5b974e03
radhikarudrawar <87385027+radhikarudrawar@users.noreply.github.com>
2022-08-05 14:23:01 +0530

    all-assessment-apis

--- src/main/java/com/RecruitPlus/quizPlatform/controller/AssessmentController.java
+++ src/main/java/com/RecruitPlus/quizPlatform/controller/AssessmentController.java
@@ -5,20 +5,40 @@ import com.RecruitPlus.quizPlatform.service.AssessmentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
+import com.RecruitPlus.quizPlatform.Exception.AssessmentNotFoundException;
 
 @RestController
 @RequestMapping("/assessments/v1")
 public class AssessmentController {
     @Autowired
     private AssessmentService assessmentService;
-
-
-
-    @PostMapping("/assessemet")
+    @GetMapping("/assessment")
+    public List<Assessment> getAllAssessments() {
+        List<Assessment> assessmentList = assessmentService.getAllAssessments();
+        return assessmentList;
+        //return new ResponseEntity<>(assessmentService.getAllAssessments(), HttpStatus.OK);
+    }
+    @GetMapping("/assessment/{assessment_id}")
+    public Optional<Assessment> getAssessmentById(@PathVariable(value = "assessment_id") String assessment_id){
+        return assessmentService.getAssessmentById(assessment_id);
+    }
+    @PostMapping("/assessment")
     @ResponseStatus(code = HttpStatus.CREATED)
     public Assessment saveAssessment(@RequestBody Assessment assessment)
     {
         Assessment saveAssessment = assessmentService.saveNewAssessment(assessment);
         return saveAssessment;
+    }
+    @ResponseStatus(code = HttpStatus.OK)
+    @PutMapping("/assessment/{assessment_id}")
+    public void  updateById(@PathVariable(value="assessment_id") String assessment_id,@RequestBody Assessment assessment )
+    {
+        assessmentService.updateAssessment(assessment_id,assessment);
+    }
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    @DeleteMapping("/assessment/{assessment_id}")
+    public void deleteAssessmentById(@PathVariable(value="assessment_id") String assessment_id) {
+            assessmentService.deleteAssessment(assessment_id);
     }
 }
--- src/main/java/com/RecruitPlus/quizPlatform/service/AssessmentService.java
+++ src/main/java/com/RecruitPlus/quizPlatform/service/AssessmentService.java
@@ -3,9 +3,11 @@ package com.RecruitPlus.quizPlatform.service;
 import com.RecruitPlus.quizPlatform.controller.AssessmentController;
 import com.RecruitPlus.quizPlatform.model.Assessment;
 import com.RecruitPlus.quizPlatform.repository.AssessmentRepository;
+import com.RecruitPlus.quizPlatform.Exception.AssessmentNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.stereotype.Service;
+import java.util.*;
 
 @Service
 public class AssessmentService {
@@ -15,9 +17,42 @@ public class AssessmentService {
    @Autowired
     MongoTemplate mongoTemplate;
 
+    //get all assessments
+    public List<Assessment> getAllAssessments(){
+        return assessmentRepository.findAll();
+    }
+    public Optional<Assessment> getAssessmentById(String assessment_id) {
+        Optional<Assessment> assessment = assessmentRepository.findById(assessment_id);
+        if(assessment.isPresent()){
+            return assessmentRepository.findById(assessment_id);
+        }
+        else{
+            throw new AssessmentNotFoundException(assessment_id);
+        }
+    }
     public Assessment saveNewAssessment(Assessment assessment)
     {
         return assessmentRepository.save(assessment);
+    }
+
+    public Optional<Assessment> updateAssessment(String assessment_id,Assessment assessment_name)
+    {
+        Assessment findById=assessmentRepository.findById(assessment_id).orElseThrow(()->new AssessmentNotFoundException(assessment_id));
+        findById.setAssessment_name(assessment_name.getAssessment_name());
+        findById.setTopics(assessment_name.getTopics());
+        findById.setCreated_by(assessment_name.getCreated_by());
+        findById.setLast_modified_by(assessment_name.getLast_modified_by());
+        assessmentRepository.save(findById);
+        return assessmentRepository.findById(assessment_id);
+    }
+    public void deleteAssessment(String assessment_id) {
+        Optional<Assessment> assessment= assessmentRepository.findById(assessment_id);
+        if(assessment.isPresent()) {
+            assessmentRepository.deleteById(assessment_id);
+        }
+        else{
+            throw new AssessmentNotFoundException(assessment_id);
+        }
     }
 
 }
